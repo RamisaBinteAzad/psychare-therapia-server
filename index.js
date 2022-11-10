42fc71c8282e13ad5b07ad893515f3b1828c796b
RamisaBinteAzad <tasnimlamia52@gmail.com>
2022-11-11 02:02:04 +0600

    removed .env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,38 +21,73 @@ async function run() {
       const serviceCollection = client
         .db("psychareTherapia")
         .collection("services");
+       const reviewsCollection = client
+         .db("psychareTherapia")
+         .collection("reviews ");
       //   Get All Services
       app.get("/services", async (req, res) => {
-       
         const query = {};
 
-          const cursor = serviceCollection.find(query);
-          //   const allServices = await cursor.toArray();
-          //  const query1 = {};
+        const cursor = serviceCollection.find(query);
+        //   const allServices = await cursor.toArray();
+        //  const query1 = {};
         // const cursor1 = serviceCollection.find(query1);
-          const services = await cursor.limit(3).toArray();
-          // console.log(services);
-           
+        const services = await cursor.limit(3).toArray();
+        // console.log(services);
+
         //   res.send({ count, services });
-          res.send(services);
-         
+        res.send(services);
+
         // res.send(services);
       });
-         app.get("/allServices", async (req, res) => {
-           const query = {};
+      app.get("/allServices", async (req, res) => {
+        const query = {};
 
-           const cursor = serviceCollection.find(query);
-           //   const allServices = await cursor.toArray();
-           //  const query1 = {};
-           // const cursor1 = serviceCollection.find(query1);
-           const allServices = await cursor.toArray();
-           console.log(allServices);
+        const cursor = serviceCollection.find(query);
+        //   const allServices = await cursor.toArray();
+        //  const query1 = {};
+        // const cursor1 = serviceCollection.find(query1);
+        const allServices = await cursor.toArray();
+        // console.log(allServices);
 
-           //   res.send({ count, services });
-           res.send(allServices);
+        //   res.send({ count, services });
+        res.send(allServices);
 
-           // res.send(services);
-         });
+        // res.send(services);
+      });
+      //   Get Specific Service
+      app.get("/services/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const service = await serviceCollection.findOne(query);
+        res.send(service);
+      });
+      app.post("/reviews", async (req, res) => {
+        const review = req.body;
+        const result = await reviewsCollection.insertOne(review);
+        res.send(result);
+      });
+     app.get("/reviews", async (req, res) => {
+       //   console.log(req.query);
+       // {} deya hoyeche sabguloke khujar jonno
+       let query = {};
+       console.log(req.query);
+
+        if (req.query.service) {
+          query = {
+            service: req.query.service,
+          };
+        }
+
+       const cursor = reviewsCollection.find(query);
+       const reviews = await cursor.toArray();
+       console.log(reviews);
+       //   toArray() - array te convert kora hoyeche client side e use er jonno
+       res.send(reviews);
+     });
+
+
+
     } finally {
   }
 }
